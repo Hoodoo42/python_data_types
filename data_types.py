fb2bfbc5fb9b0ec0f8175bf40f65f63a4dca6efa
Hoodoo42 <create@hoodooverse.ca>
2022-09-22 17:44:08 -0600

    created a list of number, looped through that list added conditionals that would print messages depedning on which conditional the number was true

--- data_types.py
+++ data_types.py
@@ -7,3 +7,12 @@ if(one.endswith('test') or two.endswith('test')):
     print('Found Test')
 else:
     print('No Test')    
+
+
+list_of_numbers = [11, 22, 36, 49, 5, 1, 16, 10]
+
+for number in list_of_numbers:
+    if(number > 10):
+        print('Large', number)
+    elif(number <= 10):
+        print('Not large', number)
