0c9c6fdf88aa138fef8199f6d7dd308083a46ee6
Hoodoo42 <create@hoodooverse.ca>
2022-09-22 18:44:21 -0600

    creating more complicated structures in python with dictionaries and lists

--- data_types.py
+++ data_types.py
@@ -16,3 +16,17 @@ for number in list_of_numbers:
         print('Large', number)
     elif(number <= 10):
         print('Not large', number)
+
+
+client = {'username': 'user1', 'age': 24, 'friends':['Anna', 'Beth', 'Carly', 'Diana']}
+
+print(client['username'], client['age'])
+
+for friend in client['friends']:
+    print(friend)
+
+
+clients = [{'username': 'user2', 'age': 26, 'friends':['Terry', 'Bob', 'Carl', 'Dan']}, {'username': 'user3', 'age': 22, 'friends':['Tracy', 'Bev', 'Cara', 'Don']}, {'username': 'user4', 'age': 32, 'friends':['Trey', 'Ben', 'Carla', 'Donna']}]
+
+for client in clients:
+    print(client['username'], client['age'], client['friends'])
